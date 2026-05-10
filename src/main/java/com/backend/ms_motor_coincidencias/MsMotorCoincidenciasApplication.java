d71edbc73968c9046cc4eeaa16af184368e8086f
Nicolas <Nicolas.vicente1226@gmail.com>
2026-05-09 23:19:34 -0400

    Feature: Agregar exclude jpa  ci al ms coincidecias

--- src/main/java/com/backend/ms_motor_coincidencias/MsMotorCoincidenciasApplication.java
+++ src/main/java/com/backend/ms_motor_coincidencias/MsMotorCoincidenciasApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration",
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration"
+})
 @EnableFeignClients
 public class MsMotorCoincidenciasApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsMotorCoincidenciasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsMotorCoincidenciasApplication.class, args);
+    }
 }
